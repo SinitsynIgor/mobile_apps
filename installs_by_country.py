5a52b9d3c1ed5d2670a23a8189b077b0a91d2b21
Igor Sinitsyn <sinitsyn.ig@gmail.com>
2020-09-29 09:09:53 +0300

    add comments

--- installs_by_country.py
+++ installs_by_country.py
@@ -15,25 +15,27 @@ from df2gspread import df2gspread as d2g
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-# получаем данные по странам из appsflyer
+# write func
 def report_geo_google_sheets(app_name, app_id):
-    ##app_name = 'Metronome_One'
-    ##app_id = 'id1428578368'
-    report_type = 'geo_by_date_report'
+    ##app_name = 'Metronome_One' # exmple app_name
+    ##app_id = 'id1428578368' #example app_id
 
+    # name report
+    report_type = 'geo_by_date_report'
+    # read token
     with open('token.json') as f:
         token = json.load(f)
 
     params = {
         'api_token': token['token'],
-        'from': '2020-01-01',
-        'to': '2020-08-31'
+        'from': '2020-01-01', # date start
+        'to': '2020-08-31' # date end
     }
-
+    # url api appsflyer
     request_url = 'https://hq.appsflyer.com/export/{}/{}/v5'.format(app_id, report_type)
 
     res = requests.request('GET', request_url, params=params)
-
+    # send request
     if res.status_code != 200:
         if res.status_code == 404:
             print('There is a problem with the request URL. Make sure that it is correct')
@@ -44,48 +46,50 @@ def report_geo_google_sheets(app_name, app_id):
         f.write(res.text)
         f.close()
 
+    # choosing file
     for file in os.listdir():
         if file.endswith("geo_by_date_report.csv") and file.startswith(app_name):
             path = os.path.join(file)
-
+    # read file
     df_geo_by_date = pd.read_csv(path)
-
+    # selected columns
     df_installs = df_geo_by_date[['Date', 'Country', 'Installs']]
 
-    # для дальнейшего анализа загрузим коды стран
+    # loading full name countries
     url = 'https://pkgstore.datahub.io/core/country-codes/country-codes_csv/data/3b9fd39bdadd7edd7f7dcee708f47e1b/country-codes_csv.csv'
     df_country = pd.read_csv(url)
     df_country = df_country[['ISO3166-1-Alpha-2', 'official_name_en']]
     df_country = df_country.rename(columns={'ISO3166-1-Alpha-2': 'ISO3166',
                                             'official_name_en': 'full_name'})
 
-    # объединим таблицы
+    # merge tables
     df = df_installs.merge(df_country, left_on='Country', right_on='ISO3166')
 
-    # выбирае нужные столбцы
+    # selected columns
     df = df[['Date', 'full_name', 'Installs']]
 
-    # название прописными и переименовываем столбец
+    # lower name columns
     df.columns = df.columns.str.lower()
+    # rename column
     df = df.rename(columns={'full_name': 'country'})
 
-    # группируем, сортируем и делаем pivot
+    # grouping, sorting, pivot
     df_group = df.groupby(['date', 'country'], as_index=False) \
         .agg({'installs': 'sum'}) \
         .sort_values('installs', ascending=False)
-
+    # change type
     df_group['date'] = pd.to_datetime(df_group['date'])
-
+    # create columns months
     df_group['month'] = df_group.date.dt.month
-
+    # grouping by month, countries
     df_group_month = df_group.groupby(['month', 'country'], as_index=False) \
         .agg({'installs': 'sum'})
-
+    # pivot
     df_pivot = df_group_month.pivot_table(values='installs', index='country', columns='month', fill_value=0) \
         .round(2)
-
+    # sorting by min
     df_sort = df_pivot.sort_values(by=df_pivot.columns.min(), ascending=False)
-
+    # create dictionary for month
     month = {1: 'Jan',
              2: 'Feb',
              3: 'Mar',
@@ -98,10 +102,11 @@ def report_geo_google_sheets(app_name, app_id):
              10: 'Oct',
              11: 'Now',
              12: 'Dec'}
-
+    # sorting by months
     df_sort = df_sort.rename(columns=month)
     # df_sort = df_sort.style.highlight_max(axis='index')
 
+    # loading data in googlesheets
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
 
@@ -125,6 +130,7 @@ def report_geo_google_sheets(app_name, app_id):
                credentials=credentials,
                row_names=True)
 
+# Using function
 # Tuner
 report_geo_google_sheets('Tuner_One', 'id1435060008')
 report_geo_google_sheets('Tuner_guitar_ukulele_bass', 'id1507738194')
